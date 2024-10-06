4da11c517f258baca0af3dd0f75169e4bc96afee
KyleDuff1 <duff.176@buckeyemail.osu.edu>
2024-10-05 21:05:47 -0400

    Created viewOrder outline.

---
+++ store/src/components/viewOrder.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react"
+import {useLocation} from "react-router-dom"
+import { useNavigate } from 'react-router-dom';
+
+const ViewOrder = () => {
+
+    const location = useLocation()
+
+    const navigate = useNavigate()
+
+    const [order, setOrder] = useState(location.state?.order)
+
+    const confirm_Order = () => {
+        navigate('/viewConfirmation', {state: {order}})
+    }
+
+    return(
+        <div>
+            <h1>View Order</h1>
+            <div>
+                <h2>Card Number</h2>
+                <p>order.credit_card_number</p>
+
+                <h2>Expiration Date</h2>
+                <p>order.expir_date</p>
+
+                <h2>CVV</h2>
+                <p>order.cvvCode</p>
+
+                <h2>Card Holder</h2>
+                <p>order.card_holder_name</p>
+            </div>
+            <div>
+                <h2>Address 1</h2>
+                <p>order.address1</p>
+
+                <h2>Address 2</h2>
+                <p>order.address2</p>
+
+                <h2>City</h2>
+                <p>order.city</p>
+
+                <h2>State</h2>
+                <p>order.state</p>
+
+                <h2>Zip Code</h2>
+                <p>order.zip</p>
+            </div>
+            <button onClick={confirm_Order}>Confirm</button>
+        </div>
+        
+    )
+}
+
+export default ViewOrder
