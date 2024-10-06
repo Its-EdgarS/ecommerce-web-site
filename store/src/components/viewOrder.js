630f11e0db76e688bff9a482667ef2ce8239739f
KyleDuff1 <duff.176@buckeyemail.osu.edu>
2024-10-05 21:23:41 -0400

    Added items and fixed formatting of order.

--- store/src/components/viewOrder.js
+++ store/src/components/viewOrder.js
@@ -19,48 +19,48 @@ const ViewOrder = () => {
             <h1>View Order</h1>
             <div>
                 <h2>Item 1</h2>
-                <p>order.buyQuantity[0]</p>
+                <p>{order?.buyQuantity[0]}</p>
 
                 <h2>Item 2</h2>
-                <p>order.buyQuantity[1]</p>
+                <p>{order?.buyQuantity[1]}</p>
 
                 <h2>Item 3</h2>
-                <p>order.buyQuantity[2]</p>
+                <p>{order?.buyQuantity[2]}</p>
 
                 <h2>Item 4</h2>
-                <p>order.buyQuantity[3]</p>
+                <p>{order?.buyQuantity[3]}</p>
 
                 <h2>Item 5</h2>
-                <p>order.buyQuantity[4]</p>
+                <p>{order?.buyQuantity[4]}</p>
             </div>
             <div>
                 <h2>Card Number</h2>
-                <p>order.credit_card_number</p>
+                <p>{order?.credit_card_number}</p>
 
                 <h2>Expiration Date</h2>
-                <p>order.expir_date</p>
+                <p>{order?.expir_date}</p>
 
                 <h2>CVV</h2>
-                <p>order.cvvCode</p>
+                <p>{order?.cvvCode}</p>
 
                 <h2>Card Holder</h2>
-                <p>order.card_holder_name</p>
+                <p>{order?.card_holder_name}</p>
             </div>
             <div>
                 <h2>Address 1</h2>
-                <p>order.address1</p>
+                <p>{order?.address1}</p>
 
                 <h2>Address 2</h2>
-                <p>order.address2</p>
+                <p>{order?.address2}</p>
 
                 <h2>City</h2>
-                <p>order.city</p>
+                <p>{order?.city}</p>
 
                 <h2>State</h2>
-                <p>order.state</p>
+                <p>{order?.state}</p>
 
                 <h2>Zip Code</h2>
-                <p>order.zip</p>
+                <p>{order?.zip}</p>
             </div>
             <button onClick={confirm_Order}>Confirm</button>
         </div>
