018615d8261a786cb0cdc82cda0020f7f8a6f4a0
KyleDuff1 <duff.176@buckeyemail.osu.edu>
2024-10-11 23:28:14 -0400

    Added outline for aboutUs page.

---
+++ store/src/components/aboutUs.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+const ContactUs = () => {
+    return(
+        
+        <div>
+            <h1>About Our Company</h1>
+            <div>
+                <h2>Mission Statement</h2>
+                <p>
+                    Our mission is to bring the highest quality of Ohio State apparel to their fans. As Ohio state alumni outselves,
+                    we know what it's like to be a fan or current student at Ohio State and want to show your pride. We want to provide
+                    those lookjing to show Ohio State pride with the high qualtiy material they deserve so they can keep rocking the 
+                    Scarlet and Gold. We live to make apparel and want to give our customer the highest quality of service.
+
+                </p>
+            </div>
+            <div>
+                <h2>Strategy</h2>
+                <p>
+                    Our strategy is simple, make the best apparel possible. We work with many indpendent retailors across the state of Ohio
+                    to get well made poliester for our products. We are constantly innovating new designs to create cool and interesting apparel
+                    not sold anywhere else. Our strategt is ti make awesome OSU branded clothing that is built to last years after you college
+                    experience.
+                </p>
+            </div>
+            <div>
+                <h2>Executives</h2>
+                <div>
+                    <h3>Edgar Sanchez</h3>
+                    <p>
+                        Position:
+                        Education: Ohio State University
+                        Experience:
+                    </p>
+                </div>
+                <div>
+                    <h3>Abril Diaz</h3>
+                    <p>
+                        Position:
+                        Education: Ohio State University
+                        Experience:
+                    </p>
+                </div>
+                <div>
+                    <h3>Kyle Duff</h3>
+                    <p>
+                        Position:
+                        Education: Ohio State University
+                        Experience:
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ContactUs;
