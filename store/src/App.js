3a8dde1b1605100f512bdbe8d120aebbdfc82abd
Edgar Sanchez <edgar.s7223@gmail.com>
2024-10-13 18:05:11 -0400

    started home page

--- store/src/App.js
+++ store/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import './App.css';
+import './stylesheets/App.css';
 import { BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom"
 import Purchase from "./components/purchase"
 import PaymentEntry from './components/paymentEntry'
 import ShippingEntry from "./components/shippingEntry"
 import ViewOrder from "./components/viewOrder"
 import ViewConfirmation from './components/viewConfirmation'
+import Home from './components/home'
+import Menu from './components/menu'
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Menu />
         <div className="content">
           <Routes>
+            <Route path="/" element={<Navigate to="/home" />} /> 
+            <Route path="/home" element={<Home />} />
             <Route path="/purchase" element={<Purchase />} />
-            <Route path="/" element={<Navigate to="/purchase" />} /> 
             <Route path="/payment" element={<PaymentEntry />} />
             <Route path="/shipping" element={<ShippingEntry />} />
-            <Route path="/vieworder" element={<ViewOrder />} />
-            <Route path="/viewconfirmation" element={<ViewConfirmation />} />
           </Routes>
         </div>
       </Router>
@@ -27,3 +29,5 @@ function App() {
 }
 
 export default App;
+            <Route path="/vieworder" element={<ViewOrder />} />
+            <Route path="/viewconfirmation" element={<ViewConfirmation />} />
