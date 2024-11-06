49fc15aab78486ef3ca8cf98bce5705a80546169
KyleDuff1 <duff.176@buckeyemail.osu.edu>
2024-11-06 02:58:26 -0500

    Commit check.

--- store/lambda_functions/order.py
+++ store/lambda_functions/order.py
@@ -16,6 +16,7 @@ def lambda_handler(event, context):
     query = "SELECT * FROM order.CUSTOMER_ORDER ORDER BY id DESC LIMIT 1;"
     cursor.execute(query)
     order = cursor.fetchall()[0]
+    print(order)
 
 
 
@@ -24,11 +25,11 @@ def lambda_handler(event, context):
     products = cursor.fetchall()
 
     query = "SELECT * FROM order.PAYMENT_INFO WHERE id = %s;"
-    cursor.execute(query, (order[3],))
+    cursor.execute(query, (order[4],))
     payment = cursor.fetchall()[0]
 
     query = "SELECT * FROM order.SHIPPING_INFO WHERE id = %s;" 
-    cursor.execute(query, (order[4],))
+    cursor.execute(query, (order[3],))
     shipping = cursor.fetchall()[0]
 
     cursor.close()
@@ -53,15 +54,11 @@ def lambda_handler(event, context):
         'order_id': order[0]
     }
 
-
-
 import json
 import mysql.connector
 
 def lambda_handler(event, context):
     data = event
-
-    incremented_value = 0
 
     cnx = mysql.connector.connect(
         host='ordersdatabase.c78oykiiortb.us-east-2.rds.amazonaws.com',
@@ -70,6 +67,7 @@ def lambda_handler(event, context):
         database='order'
     )
 
+    order_id = 0
     cursor = cnx.cursor()
 
     if data['context'] == 'checkout':
@@ -81,7 +79,6 @@ def lambda_handler(event, context):
         """
         cursor.execute(select_query)
         order_id = cursor.fetchone()[0]
-        print(order_id)
 
         insert_query = f"INSERT INTO order.PAYMENT_INFO (NAME, CARD_NUMBER, EXPIRATION_DATE, CVV) VALUES (%s, %s, %s, %s)"
         insert_values = (data['order']['firstName'], data['order']['credit_card_number'], data['order']['expir_date'], data['order']['cvvCode'])
@@ -89,11 +86,21 @@ def lambda_handler(event, context):
         cnx.commit()
         payment_key = cursor.lastrowid
 
+        cursor.execute("SELECT * FROM order.PAYMENT_INFO WHERE ID = %s", (payment_key,))
+        order_row = cursor.fetchone()
+        print(order_row)
+
+
         insert_query = f"INSERT INTO order.SHIPPING_INFO (ADDRESS_1, ADDRESS_2, CITY, STATE, ZIP) VALUES (%s, %s, %s, %s, %s)"
         insert_values = (data['order']['address1'], data['order']['address2'], data['order']['city'], data['order']['state'], data['order']['zip'])
         cursor.execute(insert_query, insert_values)
         cnx.commit()
         shipping_key = cursor.lastrowid
+
+        cursor.execute("SELECT * FROM order.SHIPPING_INFO WHERE ID = %s", (shipping_key,))
+        order_row = cursor.fetchone()
+        print(order_row)
+
 
         update_shipping_query = """
             UPDATE order.CUSTOMER_ORDER
@@ -111,11 +118,34 @@ def lambda_handler(event, context):
         cursor.execute(update_payment_query, (payment_key, order_id))
         cnx.commit()
 
+        update_payment_query = """
+            UPDATE order.CUSTOMER_ORDER
+            SET CUSTOMER_NAME = %s
+            WHERE ID = %s
+        """
+        cursor.execute(update_payment_query, (data['order']['firstName'], order_id))
+        cnx.commit()
+
+        update_payment_query = """
+            UPDATE order.CUSTOMER_ORDER
+            SET CUSTOMER_EMAIL = %s
+            WHERE ID = %s
+        """
+        cursor.execute(update_payment_query, (data['order']['email'], order_id))
+        cnx.commit()
+
+        cursor.execute("SELECT * FROM order.CUSTOMER_ORDER WHERE ID = %s", (order_id,))
+        order_row = cursor.fetchone()
+        print(order_row)
+
+
     elif data['context'] == "apparel":
-        insert_query = f"INSERT INTO order.CUSTOMER_ORDER (CUSTOMER_NAME, CUSTOMER_EMAIL, SHIPPING_INFO_ID_FK, PAYMENT_INFO_ID_FK) VALUES (%s, %s, %s, %s)"
-        insert_values = (data['order']['firstName'], data['order']['email'], shipping_key, payment_key)
+        insert_query = f"INSERT INTO order.CUSTOMER_ORDER () VALUES ()"
+        insert_values = ()
         cursor.execute(insert_query, insert_values)
         cnx.commit()
+
+        order_id = cursor.lastrowid
 
         
         for key, value in data['order']['buyQuantity'].items():
@@ -130,6 +160,7 @@ def lambda_handler(event, context):
     cnx.close()
 
     return {
+        'body': order_id,
         'statusCode': 200,
         'headers': {'Access-Control-Allow-Origin':'*', 'Access-Control-Allow-Headers': 'Content-Type'},
     }
