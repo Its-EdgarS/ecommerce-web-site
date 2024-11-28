eb516549b48f6fb8d3e51d600be6852f81c2770d
Abril Diaz <abrilalison@icloud.com>
2024-11-27 23:36:34 -0500

    added user input checking

--- store/src/components/apparel.js
+++ store/src/components/apparel.js
@@ -1,84 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios'
-import ShoppingCart from './shoppingCart';
-import '../stylesheets/apparel.css';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import ShoppingCart from "./shoppingCart";
+import "../stylesheets/apparel.css";
 
 const Apparel = () => {
     // hard coded the order object
     const [order, setOrder] = useState({
-        buyQuantity: [0, 0, 0, 0, 0], 
+        buyQuantity: [0, 0, 0, 0, 0],
         items: [],
-
-    })
-    const [id, setId] = useState()
-    const navigate = useNavigate()
+    });
+    const [id, setId] = useState();
+    const navigate = useNavigate();
     const [cart, setCart] = useState([]);
 
-    const [allApparel, setApparel] = useState([])
+    const [allApparel, setApparel] = useState([]);
     useEffect(() => {
         // invoke URL
-        const apiEndpoint = 'https://f69ur8oz4a.execute-api.us-east-1.amazonaws.com/dev/inventory-management/inventory/items?category_id=2' 
+        const apiEndpoint =
+            "https://f69ur8oz4a.execute-api.us-east-1.amazonaws.com/dev/inventory-management/inventory/items?category_id=2";
         // Using axios to fetch data
-        axios.get(apiEndpoint) 
-            .then(response => {
-                const data = response.data
-                console.log(data)
-                setApparel(data) // Set the featured jerseys data
+        axios
+            .get(apiEndpoint)
+            .then((response) => {
+                const data = response.data;
+                console.log(data);
+                setApparel(data); // Set the featured jerseys data
             })
-            .catch(error => console.error('Error fetch data: ', error))
-    }, []) //The empty dependency array [] ensures that the effect runs only once.
+            .catch((error) => console.error("Error fetch data: ", error));
+    }, []); //The empty dependency array [] ensures that the effect runs only once.
 
     // runs when pay button is clicked
     // trasnfers the order object to the payment page
     const handleSubmit = (e) => {
         e.preventDefault();
         // invoke URL
-        const apiEndpoint = 'https://41e3xst1h2.execute-api.us-east-2.amazonaws.com/dev/order-processing/order' 
+        const apiEndpoint =
+            "https://41e3xst1h2.execute-api.us-east-2.amazonaws.com/dev/order-processing/order";
         // Using axios to fetch data
         const payload = {
             context: "apparel",
-            order: order       
+            order: order,
         };
-        axios.post(apiEndpoint, payload) 
-            .then(response => {
-                const data = response.data
+        axios
+            .post(apiEndpoint, payload)
+            .then((response) => {
+                const data = response.data;
                 navigate("/checkout");
             })
-            .catch(error => console.error('Error fetch data: ', error))
-    
-        
-      };
+            .catch((error) => console.error("Error fetch data: ", error));
+    };
 
     // runs when the user enters a quantity
     const handleInputChange = (index, value) => {
-        const newBuyQuantity = {...order.buyQuantity} // get copy of the buyQuantity array
-        newBuyQuantity[index] = value
-        setOrder({...order, buyQuantity: newBuyQuantity}) // update the order
-
-        // Update the cart if the item is already in the cart
-        const newCart = [...cart];
-        const existingItemIndex = newCart.findIndex(item => item.index === index);
-        if (existingItemIndex !== -1) {
-            newCart[existingItemIndex].quantity = parseInt(value, 10);
-            setCart(newCart);
-        }
-    }
+        const sanitizedValue = Math.max(0, parseInt(value, 10) || 0);
+        const newBuyQuantity = { ...order.buyQuantity }; 
+        newBuyQuantity[index] = sanitizedValue;
+        setOrder({ ...order, buyQuantity: newBuyQuantity });
+    };
 
     const addToCart = (index) => {
-        const newCart = [...cart];
-        const existingItemIndex = newCart.findIndex(item => item.index === index);
-        if (existingItemIndex !== -1) {
-            newCart[existingItemIndex].quantity += order.buyQuantity[index];
-        } else {
-            newCart.push({
-                index,
-                image: allApparel[String(index)]['image_url'],
-                price: allApparel[String(index)]['price'],
-                quantity: order.buyQuantity[index]
-            });
+        if (order.buyQuantity[index] > 0) {
+            const newCart = [...cart];
+            const existingItemIndex = newCart.findIndex(
+                (item) => item.index === index
+            );
+            if (existingItemIndex !== -1) {
+                newCart[existingItemIndex].quantity += order.buyQuantity[index];
+            } else {
+                newCart.push({
+                    index,
+                    image: allApparel[String(index)]["image_url"],
+                    price: allApparel[String(index)]["price"],
+                    quantity: order.buyQuantity[index],
+                });
+            }
+            setCart(newCart);
         }
-        setCart(newCart);
     };
 
     const updateQuantity = (index, quantity) => {
@@ -87,7 +85,7 @@ const Apparel = () => {
         setCart(newCart);
 
         // Update the order buyQuantity
-        const newBuyQuantity = {...order.buyQuantity};
+        const newBuyQuantity = { ...order.buyQuantity };
         newBuyQuantity[cart[index].index] = quantity;
         setOrder({ ...order, buyQuantity: newBuyQuantity });
     };
@@ -97,42 +95,58 @@ const Apparel = () => {
         setCart(newCart);
 
         // Update the order buyQuantity
-        const newBuyQuantity = {...order.buyQuantity};
+        const newBuyQuantity = { ...order.buyQuantity };
         newBuyQuantity[cart[index].index] = 0; // reset the quantity to 0
         setOrder({ ...order, buyQuantity: newBuyQuantity });
     };
 
     return (
-        
-        <div> 
-            <div className='apparel-banner'>
+        <div>
+            <div className="apparel-banner">
                 <h2>All Apparel</h2>
                 <p>Enter the quantity of products you want to buy</p>
             </div>
-            <div className='apparel-container'>
+            <div className="apparel-container">
                 <form onSubmit={handleSubmit}>
-                    <div className='product-grid'>
-                    {allApparel.map((item, index) => (
-                            <div className='product-item' key={index}>
-                                <img src={item.image_url} alt={item.name} width="100" />
-                                <label>{item.description} - ${item.price}</label>
-                                <input
-                                    type='number'
-                                    value={order.buyQuantity[index]}
-                                    onChange={(e) => handleInputChange(index, e.target.value)}
+                    <div className="product-grid">
+                        {allApparel.map((item, index) => (
+                            <div className="product-item" key={index}>
+                                <img
+                                    src={item.image_url}
+                                    alt={item.name}
+                                    width="100"
                                 />
-                                <button type="button" onClick={() => addToCart(index)}>Add to Cart</button>
+                                <label>
+                                    {item.description} - ${item.price}
+                                </label>
+                                <input
+                                    type="number"
+                                    value={order.buyQuantity[index]}
+                                    min="0"
+                                    onChange={(e) =>
+                                        handleInputChange(index, e.target.value)
+                                    }
+                                />
+                                <button
+                                    type="button"
+                                    onClick={() => addToCart(index)}
+                                >
+                                    Add to Cart
+                                </button>
                                 <br />
                             </div>
                         ))}
                     </div>
-                    <ShoppingCart cart={cart} updateQuantity={updateQuantity} removeItem={removeItem}/>
-                    <button className='cart-btn'>Checkout</button>
+                    <ShoppingCart
+                        cart={cart}
+                        updateQuantity={updateQuantity}
+                        removeItem={removeItem}
+                    />
+                    <button className="cart-btn">Checkout</button>
                 </form>
             </div>
         </div>
-    
-    )
-}
+    );
+};
 
 export default Apparel;
--- store/src/components/checkout.js
+++ store/src/components/checkout.js
@@ -1,173 +1,301 @@
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import axios from 'axios'
-import "../stylesheets/checkout.css"; 
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
+import "../stylesheets/checkout.css";
 
 const CheckoutPage = () => {
+    const [order, setOrder] = useState({});
+    const [errors, setErrors] = useState({}); // Validation error state
+    const location = useLocation();
+    const navigate = useNavigate();
+    const [id, setId] = useState();
 
-  const location = useLocation();
-  const navigate = useNavigate();
-  const [id, setId] = useState()
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
 
-  const [order, setOrder] = useState();
-  
+        // Update the order state
+        setOrder({ ...order, [name]: value });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // invoke URL
-    const apiEndpoint = 'https://41e3xst1h2.execute-api.us-east-2.amazonaws.com/dev/order-processing/order' 
-    // Using axios to fetch data
-    const payload = {
-        context: "checkout",
-        order: order       
+        // Validate the input field
+        validateInput(name, value);
     };
-    axios.post(apiEndpoint, payload) 
-        .then(response => {
-            const data = response.data
-            navigate("/viewOrder");
-        })
-        .catch(error => console.error('Error fetch data: ', error))
 
-    
-  };
+    const validateInput = (name, value) => {
+        let error = "";
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setOrder({ ...order, [name]: value });
-  };
+        switch (name) {
+            case "email":
+                error = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)
+                    ? ""
+                    : "Please enter a valid email address.";
+                break;
+            case "phone":
+                error = /^\d{10}$/.test(value)
+                    ? ""
+                    : "Phone number must be 10 digits.";
+                break;
+            case "credit_card_number":
+                error = /^\d{16}$/.test(value)
+                    ? ""
+                    : "Credit card number must be 16 digits.";
+                break;
+            case "expir_date":
+                const today = new Date();
+                const selectedDate = new Date(value);
+                error =
+                    selectedDate > today
+                        ? ""
+                        : "Expiration date must be in the future.";
+                break;
+            case "cvvCode":
+                error = /^\d{3,4}$/.test(value)
+                    ? ""
+                    : "CVV code must be 3 or 4 digits.";
+                break;
+            case "zip":
+                error = /^\d{5}$/.test(value)
+                    ? ""
+                    : "Zip code must be 5 digits.";
+                break;
+            case "firstName":
+            case "lastName":
+            case "city":
+            case "state":
+            case "address1":
+            case "card_holder_name":
+                error = value.trim() === "" ? "This field is required." : "";
+                break;
+            default:
+                break;
+        }
 
-  return (
-    <div className="checkout">
-      <div className="checkout-container">
-        <form onSubmit={handleSubmit}>
-          <div className="checkout-section">
-            <h2>Contact</h2>
-            <label>{id}</label>
-            <input
-              type="email"
-              name="email"
-              required
-              onChange={(e) => handleInputChange(e)}
-            />
-          </div>
+        // Update the errors state
+        setErrors({ ...errors, [name]: error });
+    };
 
-          <div className="checkout-section">
-            <h2>Delivery</h2>
-            <div className="half-width-container">
-              <div className="half-width">
-                <label>First Name</label>
-                <input
-                  type="text"
-                  name="firstName"
-                  required
-                  onChange={(e) => handleInputChange(e)}
-                />
-              </div>
-              <div className="half-width">
-                <label>Last Name</label>
-                <input
-                  type="text"
-                  name="lastName"
-                  required
-                  onChange={(e) => handleInputChange(e)}
-                />
-              </div>
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Validate all fields on form submission
+        const newErrors = {};
+        Object.keys(order).forEach((key) => {
+            const value = order[key];
+            newErrors[key] = validateInput(key, value); // Set validation result for each field
+        });
+
+        // Set validation errors to state
+        setErrors(newErrors);
+
+        // Check for existing errors
+        if (Object.values(newErrors).some((error) => error)) {
+            console.log("Fix validation errors first.", newErrors);
+            return;
+        }
+
+        // If no errors, proceed with form submission
+        const apiEndpoint =
+            "https://41e3xst1h2.execute-api.us-east-2.amazonaws.com/dev/order-processing/order";
+        const payload = {
+            context: "checkout",
+            order: order,
+        };
+
+        axios
+            .post(apiEndpoint, payload)
+            .then((response) => {
+                console.log("Order submitted successfully:", response.data);
+                navigate("/viewOrder");
+            })
+            .catch((error) => console.error("Error fetching data:", error));
+    };
+
+    return (
+        <div className="checkout">
+            <div className="checkout-container">
+                <form onSubmit={handleSubmit}>
+                    {/* Contact Section */}
+                    <div className="checkout-section">
+                        <h2>Contact</h2>
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            required
+                            onChange={handleInputChange}
+                        />
+                        {errors.email && (
+                            <span className="error">{errors.email}</span>
+                        )}
+                    </div>
+
+                    {/* Delivery Section */}
+                    <div className="checkout-section">
+                        <h2>Delivery</h2>
+                        <div className="half-width-container">
+                            <div className="half-width">
+                                <label>First Name</label>
+                                <input
+                                    type="text"
+                                    name="firstName"
+                                    required
+                                    onChange={handleInputChange}
+                                />
+                                {errors.firstName && (
+                                    <span className="error">
+                                        {errors.firstName}
+                                    </span>
+                                )}
+                            </div>
+                            <div className="half-width">
+                                <label>Last Name</label>
+                                <input
+                                    type="text"
+                                    name="lastName"
+                                    required
+                                    onChange={handleInputChange}
+                                />
+                                {errors.lastName && (
+                                    <span className="error">
+                                        {errors.lastName}
+                                    </span>
+                                )}
+                            </div>
+                        </div>
+                        <label>Address</label>
+                        <input
+                            type="text"
+                            name="address1"
+                            required
+                            onChange={handleInputChange}
+                        />
+                        {errors.address1 && (
+                            <span className="error">{errors.address1}</span>
+                        )}
+                        <label>Apartment, Suite, etc.</label>
+                        <input
+                            type="text"
+                            name="address2"
+                            onChange={handleInputChange}
+                        />
+                        <div className="half-width-container">
+                            <div className="half-width">
+                                <label>City</label>
+                                <input
+                                    type="text"
+                                    name="city"
+                                    required
+                                    onChange={handleInputChange}
+                                />
+                                {errors.city && (
+                                    <span className="error">{errors.city}</span>
+                                )}
+                            </div>
+                            <div className="half-width">
+                                <label>State</label>
+                                <input
+                                    type="text"
+                                    name="state"
+                                    required
+                                    onChange={handleInputChange}
+                                />
+                                {errors.state && (
+                                    <span className="error">
+                                        {errors.state}
+                                    </span>
+                                )}
+                            </div>
+                            <div className="half-width">
+                                <label>Zip Code</label>
+                                <input
+                                    type="text"
+                                    name="zip"
+                                    required
+                                    onChange={handleInputChange}
+                                />
+                                {errors.zip && (
+                                    <span className="error">{errors.zip}</span>
+                                )}
+                            </div>
+                        </div>
+                        <label>Phone</label>
+                        <input
+                            type="text"
+                            name="phone"
+                            required
+                            onChange={handleInputChange}
+                        />
+                        {errors.phone && (
+                            <span className="error">{errors.phone}</span>
+                        )}
+                    </div>
+
+                    {/* Payment Section */}
+                    <div className="checkout-section">
+                        <h2>Payment</h2>
+                        <label>Credit Card Number</label>
+                        <input
+                            type="text"
+                            name="credit_card_number"
+                            required
+                            onChange={handleInputChange}
+                        />
+                        {errors.credit_card_number && (
+                            <span className="error">
+                                {errors.credit_card_number}
+                            </span>
+                        )}
+                        <div className="half-width-container">
+                            <div className="half-width">
+                                <label>Expiration Date</label>
+                                <input
+                                    type="date"
+                                    name="expir_date"
+                                    required
+                                    min={new Date().toISOString().split("T")[0]}
+                                    onChange={handleInputChange}
+                                />
+                                {errors.expir_date && (
+                                    <span className="error">
+                                        {errors.expir_date}
+                                    </span>
+                                )}
+                            </div>
+                            <div className="half-width">
+                                <label>CVV Code</label>
+                                <input
+                                    type="text"
+                                    name="cvvCode"
+                                    required
+                                    onChange={handleInputChange}
+                                />
+                                {errors.cvvCode && (
+                                    <span className="error">
+                                        {errors.cvvCode}
+                                    </span>
+                                )}
+                            </div>
+                        </div>
+                        <label>Card Holder Name</label>
+                        <input
+                            type="text"
+                            name="card_holder_name"
+                            required
+                            onChange={handleInputChange}
+                        />
+                        {errors.card_holder_name && (
+                            <span className="error">
+                                {errors.card_holder_name}
+                            </span>
+                        )}
+                    </div>
+
+                    <button type="submit" className="checkout-btn">
+                        Submit Order
+                    </button>
+                </form>
             </div>
-            <label>Address</label>
-            <input
-              type="text"
-              name="address1"
-              required
-              onChange={(e) => handleInputChange(e)}
-            />
-            <label>Apartment, Suite, etc.</label>
-            <input
-              type="text"
-              name="address2"
-              onChange={(e) => handleInputChange(e)}
-            />
-            <div className="half-width-container">
-              <div className="half-width">
-                <label>City</label>
-                <input
-                  type="text"
-                  name="city"
-                  required
-                  onChange={(e) => handleInputChange(e)}
-                />
-              </div>
-              <div className="half-width">
-                <label>State</label>
-                <input
-                  type="text"
-                  name="state"
-                  required
-                  onChange={(e) => handleInputChange(e)}
-                />
-              </div>
-              <div className="half-width">
-                <label>Zip Code</label>
-                <input
-                  type="text"
-                  name="zip"
-                  required
-                  onChange={(e) => handleInputChange(e)}
-                />
-              </div>
-            </div>
-            <label>Phone</label>
-            <input
-              type="text"
-              name="phone"
-              required
-              onChange={(e) => handleInputChange(e)}
-            />
-          </div>
-
-          <div className="checkout-section">
-            <h2>Payment</h2>
-            <label>Credit Card Number</label>
-            <input
-              type="text"
-              name="credit_card_number"
-              required
-              onChange={(e) => handleInputChange(e)}
-            />
-            <div className="half-width-container">
-              <div className="half-width">
-                <label>Expiration Date</label>
-                <input
-                  type="date"
-                  name="expir_date"
-                  required
-                  onChange={(e) => handleInputChange(e)}
-                />
-              </div>
-              <div className="half-width">
-                <label>CVV Code</label>
-                <input
-                  type="number"
-                  name="cvvCode"
-                  required
-                  onChange={(e) => handleInputChange(e)}
-                />
-              </div>
-            </div>
-            <label>Card Holder Name</label>
-            <input
-              type="text"
-              name="card_holder_name"
-              required
-              onChange={(e) => handleInputChange(e)}
-            />
-          </div>
-
-          <button className="checkout-btn">Submit Order</button>
-        </form>
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default CheckoutPage;
